4082b8db7d8397f3645e7bc94ef3159aa0e76d92
Jbak12 <kubokik5@gmail.com>
2023-02-14 04:43:42 +0100

    removed comments from ex 10

--- Connector/src/Connector.java
+++ Connector/src/Connector.java
@@ -8,49 +8,40 @@ public class Connector implements NetConnection {
         try(Socket socket = new Socket(host, port);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));){
-
             out.println("Program");
             String sLine;
-            int count = 0;
-            int result = 0;
+            int count = 0, result = 0;
             String lookingFor = "";
 
             while ((sLine = in.readLine()) != null ) {
                 if(sLine.equals("EOD")) {
                     break;
                 }
-                System.out.println(Integer.toString(count) + " " + sLine);
                 sLine = sLine.replaceAll("[^0-9]", "");
                 if(count == 3) {
                     lookingFor = sLine;
-                }
-                if (count > 3){
+                } else if (count > 3){
                     result = lookingFor.equals(sLine) ? result + 1 : result;
                 }
                 count = count + 1;
             }
             out.println(result);
-            // // System.out.println("Znaleziono " + result + " liczby " + lookingFor);
-
-            // while((sLine = in.readLine()) != null){
-            //     System.out.println("PO PROGRAMIE: " + sLine);
-            // }
-
             in.close();
             out.close();
             socket.close();
 
-
         } catch(UnknownHostException ex){
-            System.out.println("Server not found: " + ex.getMessage());
-        }catch(IOException ex){
-            System.out.println("I/O error: " + ex.getMessage());
+            System.out.println("Couldnt find server: " + ex.getMessage());
+        } catch ( ConnectException exc ) {
+            System.out.println( "Couldnt connect to " + host );
+        } catch(IOException ex){
+            System.out.println("i/o stream error: " + ex.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        // System.out.println("siema siema");
-        Connector connector = new Connector();
-        connector.connect("172.30.24.101", 8080);
-    }
+    // public static void main(String[] args) {
+    //     // System.out.println("haha serwer");
+    //     Connector connector = new Connector();
+    //     connector.connect("172.30.24.101", 8080);
+    // }
 }
